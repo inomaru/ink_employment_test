465f34f7cb45889713c733cdc7f7470e30a5452c
INO <ink.avc@gmail.com>
2023-12-25 21:18:21 +0900

    [Add] #4 Docker,Mysqlでrailsアプリを作成

---
+++ src/startup.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+
+# yarnパッケージ管理ツールをインストール
+apt-get update && apt-get install -y curl apt-transport-https wget && \
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+apt-get update && apt-get install -y yarn
+
+# webpacker
+rails webpacker:install
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+rails db:create
+rails db:migrate
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
