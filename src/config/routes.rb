3c0e8f50baeeb13235e6cc7c075d474d4d466949
INO <ink.avc@gmail.com>
2023-12-26 11:14:23 +0900

    [Add] #8 ルートにアクセスするとログインページにリダイレクト

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#top'
   get '/hello_world', to: 'application#hello'
   get '/api/hello_world', to: 'application#api_hello'
+  devise_for :users
 end
