953f8f6f9e3681941f85b818e0394dc80e3e091b
INO <ink.avc@gmail.com>
2023-12-26 09:50:49 +0900

    [Add] #6 /api/hello_worldにアクセスしてjsonを取得できる

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
     def hello
         render inline: '<h1>Hello,World</h1>'
     end
+    def api_hello
+        render json: {"message": "Hello,World", status: 200}
+    end
 end
