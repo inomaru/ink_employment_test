346d67a99cf354fee9c0a8b383dfa9355aa373d3
INO <ink.avc@gmail.com>
2023-12-26 13:50:04 +0900

    [Update] #8 インデントを2に揃える

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:api_hello, :register]
-    def top
-        render 'layouts/top'
+  before_action :authenticate_user!, except: [:api_hello, :register]
+  def top
+    render 'layouts/top'
+  end
+  def hello
+    render 'layouts/top'
+  end
+  def api_hello
+    render json: {"message": "Hello,World", status: 200}
+  end
+  def register
+    if user_signed_in?
+      redirect_to root_url
+    else
+      redirect_to new_user_registration_url
     end
-    def hello
-        render 'layouts/top'
+  end
+  def logout
+    if user_signed_in?
+      redirect_to destroy_user_session_url(current_user)
+    else
+      redirect_to new_user_registration_url
     end
-    def api_hello
-        render json: {"message": "Hello,World", status: 200}
-    end
-    def register
-        if user_signed_in?
-            redirect_to root_url
-        else
-            redirect_to new_user_registration_url
-        end
-    end
-    def logout
-        if user_signed_in?
-            redirect_to destroy_user_session_url(current_user)
-        else
-            redirect_to new_user_registration_url
-        end
-    end
+  end
 end
