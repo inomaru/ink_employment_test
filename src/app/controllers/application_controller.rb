0973cfdd6897b3b6da69f8ecc631eb0ff14efd4a
INO <ink.avc@gmail.com>
2023-12-26 09:43:51 +0900

    [Add] #6 hello_worldにアクセスできる.

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render inline: '<h1>Hello,World</h1>'
+    end
 end
---
+++ src/test/controllers/get_hello_worlds_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class GetHelloWorldsTest < ActionDispatch::IntegrationTest
+  test "hello_worldにアクセスできる" do
+    get '/hello_world'
+    assert_response :success
+  end
+  test "hello_worldに適切な文字列が表示されている" do
+    get '/hello_world'
+    assert_select "h1", { count: 1, text: 'Hello,World' }
+  end
+end
