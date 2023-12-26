a766e3715e230701bc230c0fea57be6021bccb6a
INO <ink.avc@gmail.com>
2023-12-26 09:53:16 +0900

    [Update] #6 テストファイルとコードを見やすく修正.

--- src/test/controllers/get_api_hello_world_test.rb
+++ src/test/controllers/get_api_hello_world_test.rb
@@ -8,6 +8,9 @@ class GetApiHelloWorldTest < ActionDispatch::IntegrationTest
   test "/api/hello_worldからjson形式て適切な文字列が取得できる" do
     get '/api/hello_world'
     res = JSON.parse(response.body)
-    assert_equal('Hello,World', res['message'])
+    assert_equal(
+        'Hello,World',
+        res['message']
+    )
   end
 end
--- src/test/controllers/get_hello_world_test.rb
+++ src/test/controllers/get_hello_world_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
-class GetHelloWorldsTest < ActionDispatch::IntegrationTest
+class GetHelloWorldTest < ActionDispatch::IntegrationTest
   test "hello_worldにアクセスできる" do
     get '/hello_world'
     assert_response :success
   end
   test "hello_worldに適切な文字列が表示されている" do
     get '/hello_world'
-    assert_select "h1", { count: 1, text: 'Hello,World' }
+    assert_select"h1", {
+      count: 1,
+      text: 'Hello,World'
+    }
   end
 end
