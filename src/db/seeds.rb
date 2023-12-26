e15529d59699d873ae7abb2ad923fe551b28954c
INO <ink.avc@gmail.com>
2023-12-26 13:46:08 +0900

    [Add] #8 admin権限を持つユーザーはコンテナ起動時に０人だと自動で作成される

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if User.where(auth_level: 'admin').count == 0
+  User.create([
+    {
+      email: 'first_admin_user@example.com',
+      password: 'Password111111??!',
+      auth_level: 'admin',
+    }
+  ])
+end
