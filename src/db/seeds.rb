5403401dc8bd8d6a92782ccb7537261ac4a9c3e1
INO <ink.avc@gmail.com>
2023-12-29 16:02:39 +0900

    [Add] #10 コンテナ起動時にssedファイルで40名を作成

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -8,6 +8,14 @@ if User.where(auth_level: 'admin').count == 0
     {
       email: 'un_admin_user@example.com',
       password: 'Password111111??!',
-    }
+    },
   ])
+end
+# 最大ユーザー数40人から上記の人数を引いた数
+38.times do |n|
+  User.create(
+      email: "user_#{n}@example.com",
+      password: 'Password111111??!',
+      auth_level: "#{n%2==0 ? 'admin' : 'user'}",
+   )
 end
