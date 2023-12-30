84ebca6dd123cf8649deca9dd79d241d4b83ea79
INO <ink.avc@gmail.com>
2023-12-30 16:09:51 +0900

    [Add] #8 deviseをインストール

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, except: [:hello, :api_hello]
+    def top
+        render inline: '<h1>Top page.</h1>'
+    end
     def hello
         render inline: '<h1>Hello,World</h1>'
     end
